8d5b97100789ac2626880a9dddd09b729f4b8ed7
stryker-idk <alexanderprib10@gmail.com>
2026-03-30 22:40:48 +0200

    moved debug_value over to its own class, also renamed it to debug_type
    moved debug_value over to its own class, also renamed it to debug_type

--- src/main/java/net/Stryker/VoxelPhysicsAPI/VoxelPhysicsAPI.java
+++ src/main/java/net/Stryker/VoxelPhysicsAPI/VoxelPhysicsAPI.java
@@ -1,5 +1,6 @@
 package net.Stryker.VoxelPhysicsAPI;
 
+import net.Stryker.VoxelPhysicsAPI.debug.DebugPhysicsTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -28,42 +29,12 @@ public class VoxelPhysicsAPI {
         PhysicsTypeRegistry.freeze();
 
         // DEBUG: Register a test type directly if registry is empty
-        if (PhysicsTypeRegistry.count() == 0) {
-            LOGGER.warn("No physics types registered! Adding debug value type.");
-
-            PhysicsType debugValue = new PhysicsType(
-                    ResourceLocation.fromNamespaceAndPath(MOD_ID, "debug_value"),
-                    new IRuleset() {
-                        @Override
-                        public boolean tick(LongIntMap[] current, LongIntMap[] next) {
-                            // Simple pressure diffusion, spreads and decays
-                            current[0].forEach((key, value) -> {
-                                if (value <= 1) return; // Die if too low
-
-                                int nextValue = value - 1;
-                                int x = PhysicsEngine.unpackX(key);
-                                int y = PhysicsEngine.unpackY(key);
-                                int z = PhysicsEngine.unpackZ(key);
-
-                                next[0].putMax(PhysicsEngine.pack(x+1, y, z), nextValue);
-                                next[0].putMax(PhysicsEngine.pack(x-1, y, z), nextValue);
-                                next[0].putMax(PhysicsEngine.pack(x, y+1, z), nextValue);
-                                next[0].putMax(PhysicsEngine.pack(x, y-1, z), nextValue);
-                                next[0].putMax(PhysicsEngine.pack(x, y, z+1), nextValue);
-                                next[0].putMax(PhysicsEngine.pack(x, y, z-1), nextValue);
-                            });
-                            return !next[0].isEmpty(); // Return true if we have active blocks
-                        }
-                    },
-                    1, //tick interval (no shit sherlock)
-                    new String[]{"test1", "test2"}, //names of the test values, single value would just be String valueName
-                    new MergeBehavior[]{MergeBehavior.ADD, MergeBehavior.PUT_MAX} //behavior of the test values, single value would just be MergeBehavior.behavior
-            );
-
-            PhysicsTypeRegistry.addDebugType(debugValue);
-        }
+        DebugPhysicsTypes.register();
 
         LOGGER.info("VoxelPhysics API loaded. Registered types: " + PhysicsTypeRegistry.count());
+
+        PhysicsEngine testEngine = new PhysicsEngine();
+        LOGGER.info("Engine created with " + testEngine.getTypeCount() + " type slots");
 
         // Create engine AFTER we have types
         PhysicsThread.get().start();
