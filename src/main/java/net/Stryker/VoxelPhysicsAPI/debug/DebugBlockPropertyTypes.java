d18aadf5f537394d6b7e70dd05d2e1ddd5417e8f
stryker-idk <alexanderprib10@gmail.com>
2026-04-02 21:07:19 +0200

    renamed DEBUG_PROPERY to DEBUG_PROPERTY
    renamed DEBUG_PROPERY to DEBUG_PROPERTY

--- src/main/java/net/Stryker/VoxelPhysicsAPI/debug/DebugBlockPropertyTypes.java
+++ src/main/java/net/Stryker/VoxelPhysicsAPI/debug/DebugBlockPropertyTypes.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.block.Blocks;
 
 public class DebugBlockPropertyTypes {
 
-    public static final BlockPropertyType DEBUG_PROPERY = BlockPropertyRegistry.registerType(
+    public static final BlockPropertyType DEBUG_PROPERTY = BlockPropertyRegistry.registerType(
             ResourceLocation.fromNamespaceAndPath(VoxelPhysicsAPI.MOD_ID, "debug_property"),
             50, // default
             MergeBehavior.PUT_MAX
@@ -18,9 +18,9 @@ public class DebugBlockPropertyTypes {
 
     public static void register() {
         // Register some debug values
-        BlockPropertyRegistry.register(Blocks.STONE, DEBUG_PROPERY, 100);
-        BlockPropertyRegistry.register(Blocks.DIRT, DEBUG_PROPERY, 10);
-        BlockPropertyRegistry.register(Blocks.OAK_LOG, DEBUG_PROPERY, 30);
-        BlockPropertyRegistry.register(BlockTags.LOGS, DEBUG_PROPERY, 25); // Other logs get 25
+        BlockPropertyRegistry.register(Blocks.STONE, DEBUG_PROPERTY, 100);
+        BlockPropertyRegistry.register(Blocks.DIRT, DEBUG_PROPERTY, 10);
+        BlockPropertyRegistry.register(Blocks.OAK_LOG, DEBUG_PROPERTY, 30);
+        BlockPropertyRegistry.register(BlockTags.LOGS, DEBUG_PROPERTY, 25); // Other logs get 25
     }
 }
